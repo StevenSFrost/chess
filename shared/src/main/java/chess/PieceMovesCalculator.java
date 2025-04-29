a5914c7df59199da43580d5f70e092c62af31cd5
Steven <65971161+StevenSFrost@users.noreply.github.com>
2025-04-29 07:16:48 -0700

    Fixed bounds on Calculator movement check

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -6,7 +6,7 @@ public interface PieceMovesCalculator {
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
 
     default boolean isWithinBounds(ChessMove move) {
-        return !( move.getEndPosition().getColumn() > 8 | move.getEndPosition().getColumn() < 0 | move.getEndPosition().getRow() > 8 | move.getEndPosition().getRow() < 0);
+        return !( move.getEndPosition().getColumn() > 8 | move.getEndPosition().getColumn() < 1 | move.getEndPosition().getRow() > 8 | move.getEndPosition().getRow() < 1);
     }
 
     default boolean enemyPiecePresent(ChessBoard board, ChessMove move) {
